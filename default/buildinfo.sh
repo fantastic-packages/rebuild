1e11519704936f5884f2fa9941b639f61c76bc69
Anya Lin <hukk1996@gmail.com>
2026-03-02 15:43:08 +0800

    virtual hard-disk support
    (cherry picked from commit db7a9c006b294fe628e4ea6a8dcdc16075bde5c9)

--- default/buildinfo.sh
+++ default/buildinfo.sh
@@ -21,8 +21,11 @@ set_config() {
 set_config CONFIG_VERSION_DIST '"Rebuild"'
 
 # qemu / vbox
-#set_config CONFIG_QCOW2_IMAGES y
+#set_config CONFIG_ISO_IMAGES y
+set_config CONFIG_QCOW2_IMAGES y
 #set_config CONFIG_VDI_IMAGES y
+#set_config CONFIG_VHDX_IMAGES y
+set_config CONFIG_VMDK_IMAGES y
 
 # build log
 set_config CONFIG_BUILD_LOG y
